fa3f1e9198d65ad8e813cdbddf41c2dfd9efd963
AlexeyZ <alexey.zakharchenko@gteamstaff.com>
2017-01-19 17:18:06 +0530

    Commit 2

--- src/main/java/com/az/vlcoudtest/Main.java
+++ src/main/java/com/az/vlcoudtest/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public boolean doIt() {
         System.out.println("Hellow World!");
         System.out.println("Its a bit vCloudy today");
+        System.out.println("Its cool dabudee dabudaa");
 
         return true;
 
