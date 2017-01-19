8484adba9b86fefc934768b36baba13b5b529937
AlexeyZ <alexey.zakharchenko@gteamstaff.com>
2017-01-19 19:30:08 +0530

    3d commit

--- src/main/java/com/az/vlcoudtest/Main.java
+++ src/main/java/com/az/vlcoudtest/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public boolean doIt() {
         System.out.println("Hellow World!");
         System.out.println("Its a bit vCloudy today");
-        System.out.println("Its cool dabudee dabudaa");
 
         return true;
 
