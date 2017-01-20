1ae3c1a8f9a1291587f15b46d4aa7caec1b51610
AlexeyZ <alexey.zakharchenko@gteamstaff.com>
2017-01-21 02:24:33 +0530

    back to java7

--- src/main/java/com/az/vlcoudtest/Main.java
+++ src/main/java/com/az/vlcoudtest/Main.java
@@ -1,15 +1,11 @@
 package com.az.vlcoudtest;
 
-import java.util.ArrayList;
-
 public class Main {
 
     public boolean doIt() {
         System.out.println("Hellow World!");
         System.out.println("Its a bit vCloudy today");
         System.out.println("Thanks to God its Friday");
-
-        new ArrayList<>().forEach(System.out::println);
 
         return true;
     }
