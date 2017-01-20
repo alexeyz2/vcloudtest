bfb7f9f5515e11066bef83c50a32aa5ae02f1f8c
AlexeyZ <alexey.zakharchenko@gteamstaff.com>
2017-01-20 18:47:37 +0530

    4th

--- src/main/java/com/az/vlcoudtest/Main.java
+++ src/main/java/com/az/vlcoudtest/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public boolean doIt() {
         System.out.println("Hellow World!");
         System.out.println("Its a bit vCloudy today");
+        System.out.println("Thanks to God its Friday");
 
         return true;
 
