eaca68f5f52f1d6e9236e15069375be8a9e07063
AlexeyZ <alexey.zakharchenko@gteamstaff.com>
2017-01-26 01:13:26 +0530

    Java7 strikes back

--- src/main/java/com/az/vlcoudtest/Main.java
+++ src/main/java/com/az/vlcoudtest/Main.java
@@ -1,15 +1,11 @@
 package com.az.vlcoudtest;
 
-import java.util.Collections;
-
 public class Main {
 
     public boolean doIt() {
         System.out.println("Hellow World!");
         System.out.println("Its a bit vCloudy today");
         System.out.println("Thanks to God its Friday");
-
-        Collections.emptyList().forEach(System.out::println);
 
         return true;
     }
