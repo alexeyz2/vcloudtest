a81cf0a9d0348d2e7fbe5ba3fb7b69dc6737ffa1
AlexeyZ <alexey.zakharchenko@gteamstaff.com>
2017-02-10 18:43:48 +0300

    change

--- src/main/java/com/az/vlcoudtest/Main.java
+++ src/main/java/com/az/vlcoudtest/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println("Its a bit vCloudy today");
         System.out.println("Thanks to God its Friday");
         System.out.println("vCloud knows how to wait...");
-        System.out.println("123");
+        System.out.println("1234");
 
         return true;
     }
