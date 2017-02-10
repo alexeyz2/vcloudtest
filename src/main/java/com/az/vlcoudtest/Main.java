660c745d14447e3050a3ded33d2841fdfa4b11bc
AlexeyZ <alexey.zakharchenko@gteamstaff.com>
2017-02-10 22:38:19 +0300

    Change N

--- src/main/java/com/az/vlcoudtest/Main.java
+++ src/main/java/com/az/vlcoudtest/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("Hellow World!");
         System.out.println("Its a bit vCloudy today");
         System.out.println("Thanks to God its Friday");
+
         System.out.println("vCloud knows how to wait...");
 
         return true;
