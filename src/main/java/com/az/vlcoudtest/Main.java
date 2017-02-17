f8ea9f66c0d2f96971e40103ee40d811d7174587
vladanpetrovic <vladan.petrovic@aurea.com>
2017-02-17 18:32:38 +0100

    on prem test

--- src/main/java/com/az/vlcoudtest/Main.java
+++ src/main/java/com/az/vlcoudtest/Main.java
@@ -22,7 +22,7 @@ public class Main {
         System.out.println("Thanks to God its Friday");
 
         System.out.println("vCloud knows how to wait...");
-        System.out.println("on-premise...");
+        System.out.println("on-premise commit...");
         return true;
     }
 
