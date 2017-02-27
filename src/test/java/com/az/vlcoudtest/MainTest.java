1892eb7ea0aa0e7c53b0c34602f1fa904918f75e
AlexeyZ <alexey.zakharchenko@gteamstaff.com>
2017-02-27 18:04:28 +0300

    Some hava 6 and 8 features

--- src/test/java/com/az/vlcoudtest/MainTest.java
+++ src/test/java/com/az/vlcoudtest/MainTest.java
@@ -1,12 +1,18 @@
 package com.az.vlcoudtest;
 
 
+import com.sun.tools.javac.main.JavaCompiler;
 import org.junit.Assert;
+
+import static org.hamcrest.CoreMatchers.startsWith;
 
 public class MainTest {
     @org.junit.Test
     public void testDoIt() throws Exception {
         boolean result = new Main().doIt();
+
+        String javacV = JavaCompiler.version();
+        Assert.assertThat(javacV, startsWith("1.8"));
 
         Assert.assertTrue(result);
     }
